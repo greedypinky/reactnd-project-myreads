82206d9b33d9c360ab60d5d96fa321d9e7ca7f2d
Greedy Pinky <ritalaw04@gmail.com>
2021-08-17 21:30:23 -0700

    fixed imageLinks undefined

--- src/Book.js
+++ src/Book.js
@@ -19,17 +19,20 @@ class Book extends Component {
             <li key={id} id={id}>
             <div className="book">
                 <div className="book-top">
-                    <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail})` }}></div>
-                        <div className="book-shelf-changer">
-                            <select value={book.shelf} onChange={this.handleSelect}>
-                                <option value="move" disabled>Move to...</option>
-                                <option value="wantToRead">Want to Read</option>
-                                <option value="read">Read</option>
-                                <option value="none">None</option>
-                                <option value="currentlyReading">Currently Reading</option>
-                            </select>
-                        </div>
+                    {book.imageLinks !== undefined && (
+                    <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail})` }}>
                     </div>
+                    )}
+                    <div className="book-shelf-changer">
+                        <select value={book.shelf} onChange={this.handleSelect}>
+                            <option value="move" disabled>Move to...</option>
+                            <option value="wantToRead">Want to Read</option>
+                            <option value="read">Read</option>
+                            <option value="none">None</option>
+                            <option value="currentlyReading">Currently Reading</option>
+                        </select>
+                    </div>
+                </div>
                 <div className="book-title">{book.title}</div>
                 <div className="book-authors">{book.authors}</div>
             </div>
